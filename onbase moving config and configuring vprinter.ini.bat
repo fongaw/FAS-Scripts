a8f45417f61520833eb4e21ca1c31d873f18733c
Allen Fong <35322985+fongaw@users.noreply.github.com>
2018-03-26 15:59:26 -0700

    Update onbase moving config and configuring vprinter.ini.bat

--- onbase moving config and configuring vprinter.ini.bat
+++ onbase moving config and configuring vprinter.ini.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal enableextensions
-echo "Connecting to FAOALPHA 169.237.134.15"
-pushd \\169.237.134.15\software\Hyland Onbase\Unity13
+echo "Connecting to FAOALPHA \\\\\\\*REMOVEDFORPRIVACY*///////"
+pushd \\\\\\\*REMOVEDFORPRIVACY*///////
 echo "Installing vcredist_x86.exe"
 vcredist_x86.exe /wait
-echo "Copying obunity.exe.config from \\faoalpha\ to C:\Program Files (x86)\Hyland\Unity Client"
+echo "Copying obunity.exe.config from \\\\\\\*REMOVEDFORPRIVACY*/////// to C:\Program Files (x86)\Hyland\Unity Client"
 xcopy "obunity.exe.config" "C:\Program Files (x86)\Hyland\Unity Client"
 
-pushd \\169.237.134.15\BigFixInstaller\Hyland
-echo "Copying Hyland OnBase Virtual Print Driver x64 from \\faoalpha\ to C:\"
+pushd \\\\\\\*REMOVEDFORPRIVACY*///////
+echo "Copying Hyland OnBase Virtual Print Driver x64 from \\\\\\\*REMOVEDFORPRIVACY*/////// to C:\"
 xcopy "Hyland OnBase Virtual Print Driver x64.msi" "C:\Program Files"
 cd "C:\Program Files"
 "Hyland OnBase Virtual Print Driver x64.msi"
